dc6adbbe8091d74fb25e34926a02211d00f34127
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-16 15:03:37 -0600

    working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './Components/Nav/Nav';
+import routes from './routes';
+import { connect } from 'react-redux';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Testing Tessa's connection</p>
-      </header>
+     <Nav/>
+     {routes}
     </div>
   );
 }
+const mapStateToProps = (reduxState) => reduxState
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './reset.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './ducks/store';
+import { HashRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
