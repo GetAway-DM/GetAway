e91f71f28d3b4ee83a2cb503aa23fec993aba17c
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-10-16 17:06:05 -0500

    fixed a bug in the get_listing_by_id.sql file

--- db/get_listing_by_id.sql
+++ db/get_listing_by_id.sql
@@ -1,5 +1,5 @@
-SELECT l.title, l.city, l.state, u.first_name, u.last_name, u.profile_img, l.description, l.bedrooms, l.bathrooms FROM l listings
-JOIN u users
+SELECT l.title, l.city, l.state, u.first_name, u.last_name, u.profile_img, l.description, l.bedrooms, l.bathrooms FROM listings l
+JOIN users u
 ON l.owner_id = u.user_id
 WHERE l.listing_id = $1;
 
