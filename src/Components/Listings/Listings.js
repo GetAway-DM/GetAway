1b37bd605cf961b4ed0f86b5abf1f5424b2a5de3
Matthew Mccarthy <matthewmc96@gmail.com>
2020-10-16 20:24:30 -0500

    done with get endpoint and listing skeleton component

---
+++ src/Components/Listings/Listings.js
@@ -0,0 +1,24 @@
+
+import React, { useState, useEffect } from 'react'
+
+const Listing = (props) => {
+    const [title, setTitle]= useState('')
+    const [price, setPrice] = useState(0)
+    const [host, setHost] = useState('')
+
+
+
+    useEffect(() => {
+        axios
+            // TODO finish passing props into the axios request
+            .get(`/api/listing/getlisting/${this.state.id}`)
+            // TODO need to finish this request
+            .then(res => (res.date))
+
+    }, [] )
+
+    return (<div></div>)
+
+}
+
+export default Listing
