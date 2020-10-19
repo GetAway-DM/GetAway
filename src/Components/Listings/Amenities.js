75d74b05cd1846a51643a702878ac279077b25c3
Matthew Mccarthy <matthewmc96@gmail.com>
2020-10-19 15:46:40 -0500

    done with listng redux and functionality

---
+++ src/Components/Listings/Amenities.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Amenities() {
+    return (
+            <div>
+                <h1>Amenities</h1>
+            </div>
+    )
+}
+
+export default Amenities
+
+
+
+
