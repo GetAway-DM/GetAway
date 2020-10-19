6d11de334cfbb75e50c247333d8960b357372870
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-19 10:27:08 -0600

    create listing route

---
+++ src/Components/CreateListing/CreateListing.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class CreateListing extends Component {
+
+    render(){
+        return(
+            <div>Hi</div>
+        )
+    }
+}
+export default CreateListing
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import { Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
-import Account from './Components/Account/Account'
+import Account from './Components/Account/Account';
+import CreateListing from './Components/CreateListing/CreateListing'
 // import Dashboard from './Components/Dashboard/Dashboard';
 import Homepage from './Components/Homepage/Homepage';
 // import Listings from './Components/Listings/Listings';
