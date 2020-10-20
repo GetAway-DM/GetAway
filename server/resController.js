9712d656733e3cfefdca6df1cd6863a65286e3de
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-10-20 12:22:47 -0500

    worked on the res_controller

--- server/resController.js
+++ server/resController.js
@@ -1,8 +1,32 @@
 module.exports = {
-    createReservation: (req, res) => {
+  createReservation: async (req, res) => {
+    const db = req.app.get('db')
 
-    },
-    deleteReservation: (req, res) => {
+    const { date_from, date_to } = req.body
+    const { listing_id } = req.params
 
-    },
+    const [reservation] = await db.check_listing([listing_id, date_from, date_to])
+    if (reservation) {
+      return res.status(409).send('listing is currently booked')
+    }
+    if (date_from > date_to) {
+      return res.status(500).send(console.log('date_to cannot be earlier than date_from'))
+    } else if (date_from === null || date_to === null || date_from === '' || date_to === '') {
+      return res.status(500).send(console.log('please select both dates'))
+    } else {
+      const [reservation] = await db.create_reservation(req.session.user.user_id, listing_id, date_from, date_to)
+
+      res.status(200).send(reservation)
+    }
+  },
+  deleteReservation: async (req, res) => {
+    const db = req.app.get('db')
+    const { res_id } = req.params
+
+    await db.delete_reservation([res_id])
+
+    const getMyRes = await db.get_user_reservations([req.session.user.user_id])
+
+    res.status(200).send(getMyRes)
+  },
 }
---
+++ src/Components/Listings/Reservation.js
@@ -0,0 +1,52 @@
+import Axios from 'axios'
+import React, { useState } from 'react'
+import Datepicker from 'react-datepicker'
+import { connect } from 'react-redux'
+
+function Reservation(props) {
+  const [dateFrom, setDateFrom] = useState(new Date())
+  const [dateTo, setDateTo] = useState(new Date())
+
+  const handleReservation = () => {
+    Axios.post(`/api/reservation/newreservation/${listing_id}`, { user_id, listing_id, date_from, date_to }).then(
+      res.history.push('/')
+    )
+  }
+
+  return (
+    <div className="reserve-container">
+      <div className="info-container">
+        <p>{props.price}/night</p>
+        <div>
+          <p>star icon here</p>
+          <p>avg rating here</p>
+        </div>
+      </div>
+      <div className="selector-container">
+        <div className="calendar-selector">
+          <p>Check in</p>
+          <Datepicker selected={dateFrom} onChange={(date) => setDateFrom(date)} />
+        </div>
+        <div className="calendar-selector">
+          <p>Check out</p>
+          <Datepicker selected={dateTo} onChange={(date) => setDateTo(date)} />
+        </div>
+      </div>
+      <div className="guests-reserve">
+        <div>
+          <p>Guests</p>
+          <input className="guest-type" placeholder="under construction"></input>
+        </div>
+        <button className="reserve-button" onClick={handleReservation}>
+          Reserve
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return state
+}
+
+export default connect(mapStateToProps)(Reservation)
