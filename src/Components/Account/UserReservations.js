a90cfe1f9c3798aeeda1abb098350be5a0093c55
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-21 13:43:03 -0600

    um reservations

--- src/Components/Account/UserReservations.js
+++ src/Components/Account/UserReservations.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react'
-
+import { connect } from 'react-redux';
+import {getUser} from '../../ducks/authReducer';
 class UserReservations extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            reservations: [],
+            user_id: 0
+        }
+    }
+
+    async componentDidMount(){
+        if (!this.props.isLoggedIn) {
+            this.props.getUser().catch((err) => {
+                    this.props.history.push('/')
+                }
+            )
+        }
+        this.props.getUser().then(res => {
+            this.setState({
+                user_id: this.props.authReducer.user_id
+            })
+        })
+        }
+    
+        componentDidUpdate(prevProps){
+        if (this.props.user_id !== prevProps.user_id){
+            this.props.getUser()
+            }
+        }
+
+        
+
+
+
     render(){
-  return (
-    <div>
-      <h1>My Reservations</h1>
-    </div>
-  )
+        return (
+            <div>
+                <h1>My Reservations</h1>
+            </div>
+        )
+    }
 }
-}
-export default UserReservations
+const mapStateToProps = (state) => state
+export default connect(mapStateToProps, {getUser})(UserReservations)
