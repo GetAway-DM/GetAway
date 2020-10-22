985a45b6fa6d93ff7fd643b1726768e47b4ab064
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-22 15:07:11 -0600

    started s3

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const authCtrl = require('./authController.js')
 const listCtrl = require('./listController')
 const resCtrl = require('./resController')
 const verifyUser = require('./middlewares/verifyUser')
+const aws = require('aws-sdk');
 
 
 const app = express()
 
-const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
+const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY } = process.env
 
 app.use(express.json())
 app.use(
@@ -21,6 +23,40 @@ app.use(
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 },
   })
 )
+
+//aws s3 stuff
+app.get('/sign-s3', (req, res) => {
+
+  aws.config = {
+    region: 'us-east-1',
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+  
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+
+    return res.send(returnData)
+  });
+});
 
 //Endpoints--
 // auth endpoints
