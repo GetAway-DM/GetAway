9390633960a2648d75d27a2b6e842e0f8b022fc4
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-26 17:03:15 -0600

    woot

---
+++ db/get_listing_by_owner.sql
@@ -0,0 +1,2 @@
+SELECT * FROM listings
+WHERE owner_id = $1;
--- src/Components/Account/MyReservations.js
+++ src/Components/Account/MyReservations.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux';
 import axios from 'axios';
-import datepicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 class MyReservations extends Component{
     constructor(props){
         super(props)
@@ -24,8 +22,6 @@ class MyReservations extends Component{
             date_to,
             listing_id
         })
-        const dfrom = this.getDate(date_from)
-        console.log(dfrom)
         
         await axios.get(`/api/listing/getlisting/${listing_id}`).then((res) => {
             this.setState({
